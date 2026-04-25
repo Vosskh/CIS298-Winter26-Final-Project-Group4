f68374c1e6315e871c7d9995a91eaa6c1d71b7e1
AdamHennaoui1 <adamelh@umich.edu>
2026-04-25 18:08:38 -0400

    game logic updated

--- game_logic.py
+++ game_logic.py
@@ -1,1 +1,23 @@
+class GameLogic:
+    def __init__(self):
+        self.player1_score = 0
+        self.player2_score = 0
+        self.current_player = 1
 
+    def add_points(self, points):
+        if self.current_player == 1:
+            self.player1_score += points
+        else:
+            self.player2_score += points
+
+    def switch_turn(self):
+        if self.current_player == 1:
+            self.current_player = 2
+        else:
+            self.current_player = 1
+
+    def get_scores(self):
+        return self.player1_score, self.player2_score
+
+    def get_current_player(self):
+        return self.current_player
